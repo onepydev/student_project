7c1b7c1e741db0b310400655e85415958daf901b
netbitsolutions <henryofozor@gmail.com>
2022-11-02 12:02:20 +0100

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -2,5 +2,5 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URI=os.environ("SQLALCHEMY_DATABASE_URI")
-SECRET_KEY=os.environ("SECRET_KEY")
+SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI")
+SECRET_KEY=os.environ.get("SECRET_KEY")
